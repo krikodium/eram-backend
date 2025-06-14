ae93713ef6924fa6f21300149fe4e206f7107584
krikodium <mateokrikorian@gmail.com>
2025-06-14 04:32:58 -0300

    final arreglo

--- src/config/db.js
+++ src/config/db.js
@@ -2,14 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER,      // eramdb_user ✔
+  host: process.env.DB_HOST,      // dpg-…render.com ✔
+  database: process.env.DB_NAME,  // debería ser eramdb, NO eramdb_user
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;

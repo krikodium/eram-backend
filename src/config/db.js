c6d368dbc0f23751cbbf147a43bbb2c407527764
krikodium <mateokrikorian@gmail.com>
2025-06-14 04:20:43 -0300

    funciona al fin

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || undefined,
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
